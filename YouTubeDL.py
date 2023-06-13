9a7e46561d201e9f89477896bb839905d15d57eb
Mahdi-fatemii <fatemikiamahdi67@gmail.com>
2023-06-13 17:05:26 +0330

    Update YouTubeDL.py

--- YouTubeDL.py
+++ YouTubeDL.py
@@ -2,7 +2,8 @@ from pytube import YouTube
 from sys import argv
 
 link = argv[1]
-yt = YouTube(link)
+yt = YouTube(link) #you'll have to give the link in windows 
+#terminal with this command: 'python YoutubeDL.py "your youtube video"'
 
 print("title of the video:", yt.title)
 print("length of the video:", yt.length)
@@ -11,3 +12,5 @@ print("views of the video:", yt.views)
 yd = yt.streams.get_highest_resolution()
 
 yd.download('\Users\MT1ShotYT\Desktop\VS\dl')
+#and this is the download location you should 
+#change this according to your address
