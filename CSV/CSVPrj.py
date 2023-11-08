17e1e3a2d4fd90bc61dd72da830247fec219c1b1
Mahdi-fatemii <fatemikiamahdi67@gmail.com>
2023-11-08 08:47:31 +0330

    Update CSVPrj.py

--- CSV/CSVPrj.py
+++ CSV/CSVPrj.py
@@ -1,40 +1,87 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
-#visualization
-csvFile1 = pd.read_csv("injury-statistics-work-related-claims-2018-csv.csv")
-x = csvFile1.Year
-y = csvFile1.Value
-plt.scatter(x, y)
-plt.show()
-
-csvFile2 = pd.read_csv("serious-injury-outcome-indicators-2000-2020-CSV.csv")
-a = csvFile2.Year
-b = csvFile2.Value      
-plt.scatter(a, b)
-plt.show()
+import csv 
 
 
-csvFilediff = pd.read_csv("diff.csv")
-a = csvFilediff.Year
-b = csvFilediff.Value
-c = csvFilediff.Year
-d = csvFilediff.Value
-plt.scatter(a, b)
-plt.show()
-plt.scatter(c, d)
-plt.show()
+def column_names():
+    df = pd.read_csv('Age1.csv')
+    list_of_column_names = list(df.columns)
+    return list_of_column_names
 
 
+def visualization_csv():
+    x = []
+    y = []
+    
+    fileName = input('enter your csv file name in the directory:')
+    csvFile = open(f'{fileName}.csv', 'r')
+    lines = csv.reader(csvFile, delimiter=',') 
+    for row in lines: 
+        x.append(row[0]) 
+        y.append(row[1])
+  
+    column = column_names()
+    plt.scatter(x, y, color = 'g',s = 100) 
+    plt.xticks(rotation = 25) 
+    plt.xlabel(f'{column[0]}') 
+    plt.ylabel(f'{column[1]}') 
+    plt.title('') 
+    plt.show() 
 
 
+def visualization_csv_merged(fileName):
+    x = []
+    y = []
+    
+    csvFile = open(f'{fileName}.csv', 'r')
+    lines = csv.reader(csvFile, delimiter=',') 
+    for row in lines: 
+        x.append(row[0]) 
+        y.append(row[1])
+  
+    column = column_names()
+    plt.scatter(x, y, color = 'g',s = 100) 
+    plt.xticks(rotation = 25) 
+    plt.xlabel(f'{column[0]}') 
+    plt.ylabel(f'{column[1]}') 
+    plt.title('') 
+    plt.show() 
 
 
-#merge
-df1 = pd.read_csv("Age1.csv")
-df2 = pd.read_csv("Age2.csv")
-c_result_m = df1.merge(df2, how='outer')
-print(c_result_m)
+def merge_two_csv():
+    csvFileName = input('enter your first csv file name in the directory:')
+    csvFileName2 = input('enter your second csv file name in the directory:')
+    df1 = pd.read_csv(f'{csvFileName}.csv')
+    df2 = pd.read_csv(f'{csvFileName2}.csv')
+    c_result_m = df1.merge(df2, how='outer')
+    print(c_result_m)
+    #creating merged file
+    c_result_m.to_csv(f'merged file_{csvFileName}+{csvFileName2}.csv', index=False)
+    print(f'check the "merged file_{csvFileName}+{csvFileName2}.csv" at directory for more information ')
+    fileName =f'merged file_{csvFileName}+{csvFileName2}'
+    return fileName
 
 
+def main():
+    while True:
+        print('1: Visualization\n2: Merge\n3: Exit')
+        chk = input('select a number:')
+        if chk == '1':
+            visualization_csv()
+            continue
+        elif chk == '2':
+            fileName = merge_two_csv()
+            chk1 = input('do you want to visualise the merged file?(y or n):')
+            while True:
+                if chk1 == 'y':
+                    visualization_csv_merged(fileName)
+                    break
+                elif chk1 == 'n':
+                    break
+        elif chk == '3':
+            print('Goodbye')
+            break
+        
+        
 
+main()
